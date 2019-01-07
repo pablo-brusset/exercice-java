2a2329909e7ac3dab61e17d78f9961004384a4c6
pablo-brusset <pablo.brusset34@gmail.com>
2019-01-07 09:14:19 +0100

    exo java

---
+++ exopro1/exopro1.java
@@ -0,0 +1,21 @@
+package exopro1;
+
+import java.util.Scanner;
+
+public class exopro1 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println(" Veuillez choisir un premier chiffre: ");
+		try {
+		int i =sc.nextInt();
+		System.out.println("veuillez choisir un second chiffre: ");
+		int x =sc.nextInt();
+		System.out.println("Le resultat est: " +(i + x ));
+		}
+		catch (Exception e) {
+			System.out.println("Un chiffre ! ");
+		}
+	}
+
+}
---
+++ exopro1/exopro2.java
@@ -0,0 +1,21 @@
+package exopro1;
+
+import java.util.Scanner;
+
+public class exopro2 {
+
+	public static void main(String[] args) {
+		Scanner scan= new Scanner(System.in);
+		System.out.println("Veuillez entrer votre nom: ");	
+		String nom= scan.nextLine();
+		
+		nom=nom.toUpperCase();	
+		System.out.println("Veuillez entrer votre prenom: ");
+		
+		String prenom= scan.nextLine();
+		prenom=prenom.substring(0, 1).toUpperCase()+prenom.substring(1).toLowerCase();
+		System.out.println("votre resultat: " +nom  + " "+ prenom);
+		
+	}
+
+}
